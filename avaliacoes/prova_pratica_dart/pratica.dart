9f28a996c9171b0bab8144a5223d83140fc767c8
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-06-03 13:43:50 -0300

    Update pratica.dart

--- avaliacoes/prova_pratica_dart/pratica.dart
+++ avaliacoes/prova_pratica_dart/pratica.dart
@@ -2,31 +2,21 @@ import 'dart:io';
 
 import 'package:sqlite3/sqlite3.dart';
 
-
-
 void main() async {
 
   print('Using sqlite3 ${sqlite3.version}');
 
-
-
-  
-
   final db = sqlite3.openInMemory();
 
 
-
+  
   // Ceriando a tabela
 
   db.execute('''
-
     CREATE TABLE TB_Estudantes (
-
-      id INTEGER NOT NULL PRIMARY KEY,   
-
-      nome TEXT NOT NULL,                
-
-      idade INTEGER                      
+    id INTEGER NOT NULL PRIMARY KEY,   
+    nome TEXT NOT NULL,                
+    idade INTEGER                      
 
     );
 
@@ -35,27 +25,19 @@ void main() async {
 
 
   // Inserção assíncrona de registros
-
-  await inserirEstudante(db, 'Marina', 17);
-
+  await inserirEstudante(db, 'Yasmin', 19);
   await inserirEstudante(db, '´Fulana', 18);
-
-   await inserirEstudante(db, '´Deltraninha', 19);
+  await inserirEstudante(db, '´Deltraninha', 19);
 
 
 
   // Consulta assíncrona de registros
-
   print('\n Dados após inserção:');
-
   await consultarEstudantes(db);
 
 
-
   // Atualização assíncrona de registros
-
-  await atualizarEstudante(db, 1, 'Marina', 17);
-
+  await atualizarEstudante(db, 1, 'Yasmin', 19);
   await atualizarEstudante(db, 1, 'Fulana', 18);
 
 
@@ -64,20 +46,15 @@ void main() async {
 
   await consultarEstudantes(db);
 
-
-
+  
   // Exclusão assíncrona de registros
-
   await excluirEstudante(db, 2);
-
   print('\n Dados após exclusão:');
-
   await consultarEstudantes(db);
 
 
 
   // Encerramento do banco
-
   db.dispose();
 
 }
@@ -85,18 +62,12 @@ void main() async {
 
 
 // Função assíncrona para inserir registros
-
 Future<void> inserirEstudante(Database db, String nome, int idade) async {
-
   await Future(() {
-
-    final stmt = db.prepare('INSERT INTO TB_Estudantes (nome, idade) VALUES (?, ?)');
-
-    stmt.execute([nome, idade]);
-
-    stmt.dispose();
-
-    print('Registro inserido: $nome, $idade anos');
+   final stmt = db.prepare('INSERT INTO TB_Estudantes (nome, idade) VALUES (?, ?)');
+   stmt.execute([nome, idade]);
+   stmt.dispose();
+   print('Registro inserido: $nome, $idade anos');
 
   });
 
@@ -105,16 +76,12 @@ Future<void> inserirEstudante(Database db, String nome, int idade) async {
 
 
 // Função assíncrona para consultar registros
-
 Future<void> consultarEstudantes(Database db) async {
 
   await Future(() {
-
     final resultSet = db.select('SELECT * FROM TB_Estudantes');
-
     for (final row in resultSet) {
-
-      print('Estudante[id: ${row['id']}, nome: ${row['nome']}, idade: ${row['idade']}]');
+    print('Estudante[id: ${row['id']}, nome: ${row['nome']}, idade: ${row['idade']}]');
 
     }
 
@@ -125,13 +92,9 @@ Future<void> consultarEstudantes(Database db) async {
 
 
 // Função assíncrona para atualizar registros
-
 Future<void> atualizarEstudante(Database db, int id, String novoNome, int novaIdade) async {
-
   await Future(() {
-
     db.execute('UPDATE TB_Estudantes SET nome = ?, idade = ? WHERE id = ?', [novoNome, novaIdade, id]);
-
     print('Registro atualizado: id=$id -> $novoNome, $novaIdade anos');
 
   });
@@ -141,16 +104,11 @@ Future<void> atualizarEstudante(Database db, int id, String novoNome, int novaId
 
 
 // Função assíncrona para excluir registros
-
 Future<void> excluirEstudante(Database db, int id) async {
-
   await Future(() {
-
     db.execute('DELETE FROM TB_Estudantes WHERE id = ?', [id]);
-
-    print('Registro excluído: id=$id');
+   print('Registro excluído: id=$id');
 
   });
 
 }
-
